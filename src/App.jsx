a77d1f6c7f345467c07ef6f6dd0bb761fcaf1e54
amish1188 <martin.joodd@gmail.com>
2020-06-05 18:35:55 +0200

    add setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+    state = {  }
+    render() {
+        return (
+            <Layout>
+                <p>Hei</p>
+            </Layout>
+        );
+    }
+}
+
+export default App;
