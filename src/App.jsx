6c56341a48fa17fab233ecfd70669b8ac8a74410
amish1188 <martin.joodd@gmail.com>
2020-06-05 19:11:07 +0200

    add proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout/Layout.jsx';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
     state = {  }
     render() {
         return (
             <Layout>
-                <p>Hei</p>
+                <BurgerBuilder />
             </Layout>
         );
     }
